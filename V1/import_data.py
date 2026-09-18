54d63392798bed9f1ff8d84dc63f6d0c4547f684
Aditya Suthar <adityabishnoi576@gmail.com>
2026-09-18 22:16:58 +0530

    INSERTED into PostgreSQL(local database)

--- V1/import_data.py
+++ V1/import_data.py
@@ -1,5 +1,15 @@
 import pandas as pd
+import psycopg
 # Imported panda for tabular conversion of dataset in form of .csv
+
+
+tomato = psycopg.connect(
+    dbname = 'sports_analysis',
+    user = 'postgres',
+    password = '517204',
+    host = 'localhost',
+    port = '5432'
+)
 
 df = pd.read_csv("EPL_Data_25/football.csv")
 
@@ -7,4 +17,29 @@ football_df = df[
     ['Date', 'Time','HomeTeam', 'AwayTeam', 'FTHG', 'FTAG', 'FTR']
 ]
 
-print(football_df.head())
+football_df['Date'] = pd.to_datetime (football_df['Date'] , dayfirst=True)
+football_df['Time'] = pd.to_datetime (football_df['Time'] , format = '%H:%M').dt.time
+
+cursor = tomato.cursor()
+
+for i in range(len(football_df)):
+
+        match = football_df.iloc[i]
+        cursor.execute(
+            """
+            INSERT INTO sports_analysis 
+            (match_time,match_date,home_team,away_team,home_goals,away_goals,result)
+            VALUES(%s,%s,%s,%s,%s,%s,%s)
+            """,
+            (
+                match["Time"],
+                match["Date"],
+                match["HomeTeam"],
+                match["AwayTeam"],
+                int(match["FTHG"]),
+                int(match["FTAG"]),
+                match["FTR"]
+            )
+        )
+
+        tomato.commit()
